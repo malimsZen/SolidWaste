3ac9395a4a2e5a2786f580680b0b5dcd1984cb21
malims_zen <75028470+malimsZen@users.noreply.github.com>
2023-03-28 12:58:25 +0000

    python script defining connection to the database and uploading the data into their respective Dbs.

---
+++ solidWaste.py
@@ -0,0 +1,48 @@
+# create a function that connects to the MySQL database in the same directory
+# and returns a cursor object
+
+def connectToDB():
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", user="malims", passwd="P@$$w0rd", db="SolidWaste")
+    cursor = db.cursor()
+    return cursor
+
+# iterate through the whole folder and store each CSV file to their corresponding table in the solidWaste.sql file in the same directory
+def storeCSVtoDB():
+    import os
+    import csv
+    cursor = connectToDB()
+    for file in os.listdir(os.getcwd()):
+        if file.endswith(".csv"):
+            with open(file, 'rb') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+                for row in reader:
+                    if row[0] == 'Date':
+                        cont
+                    else:
+                        cursor.execute("INSERT INTO " + file[:-4] + " VALUES (%s, %s, %s, %s, %s, %s)", row)
+    cursor.close()
+
+# create a function that returns a list of all the tables in the solidWaste.sql file
+def getTables():
+    cursor = connectToDB()
+    cursor.execute("SHOW TABLES")
+    tables = cursor.fetchall()
+    cursor.close()
+    return tables
+    print(tables)
+
+# create a function that returns a list of all the columns in a table
+def getColumns(table):
+    cursor = connectToDB()
+    cursor.execute("SHOW COLUMNS FROM " + table)
+    columns = cursor.fetchall()
+    cursor.close()
+    return columns
+    print(columns)
+
+
+
+
+
+
