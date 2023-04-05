7056878bb856e6fe29683005855e26d6b3aac835
malims_zen <75028470+malimsZen@users.noreply.github.com>
2023-04-05 06:02:19 +0000

    Finals exam & datasets

---
+++ Business Analytics/Finals.R
@@ -0,0 +1,29 @@
+# Plot out a “Violin plot” to show relationship between “Months_on_book” and “Total_Amt_Chng_Q4_Q1” columns in the datasets in the “BankChurners” folder
+
+# Load the required packages
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(readr)
+
+# Read in the data
+data <- read_csv("BankChurners.csv")
+
+# Create a new dataframe with only the columns we want
+data <- data %>% select(Months_on_book, Total_Amt_Chng_Q4_Q1)
+
+# Create a violin plot
+ggplot(data, aes(x = Months_on_book, y = Total_Amt_Chng_Q4_Q1)) + geom_violin()
+
+# Save the plot
+ggsave("ViolinPlot.png")
+
+# Path: Business Analytics/Finals.R 
+
+
+#Using “Bank Churners" Dataset create a scatter plot using ggplot2, where each plot shows the relationship between “Months_on_book” and “Credit_Limit” and show the differnet education levels in your plot. Use facet_wrap() to arrange the plots based on Marital status. Save the plot as “ScatterPlot.png”
+
+
+
+
+
